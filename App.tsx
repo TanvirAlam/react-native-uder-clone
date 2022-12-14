63f2d640394ec1684f8420e84bb6e14e1076e3e1
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-14 20:00:11 +0100

    installing react-native-nagiation and wrapping the app

--- App.tsx
+++ App.tsx
@@ -3,15 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-
+import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
