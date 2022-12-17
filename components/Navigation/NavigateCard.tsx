90b6c25b68193bfd0caceaae10b798808271f487
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-17 23:01:18 +0100

    Updating navigation card with google place holder

--- components/Navigation/NavigateCard.tsx
+++ components/Navigation/NavigateCard.tsx
@@ -1,12 +1,66 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+import tw from "twrnc";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>NavigateCard</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to..."
+            enablePoweredByContainer={false}
+            styles={ToInputBoxStyles}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setOrigin({
+                  location: details?.geometry.location,
+                  description: data?.description,
+                })
+              );
+              navigation.navigate("RideOptionCard");
+            }}
+            fetchDetails={true}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NavigateCard;
+
+const ToInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
