036330f9d79d7948dec05c8566f0623b2b9556c3
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-17 18:47:08 +0100

    Createing a new component navigationCard

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -3,6 +3,10 @@ import React from "react";
 
 import tw from "twrnc";
 import Map from "../components/Maps/Map";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/Navigation/NavigateCard";
+
+const Stack = createNativeStackNavigator();
 
 const MapScreen = () => {
   return (
@@ -10,7 +14,15 @@ const MapScreen = () => {
       <View style={tw`h-1/2`}>
         <Map />
       </View>
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
