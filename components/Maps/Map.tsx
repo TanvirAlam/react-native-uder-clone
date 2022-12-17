03cc41704476c1e3e19edee812b29f4cd8b04e6d
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-18 00:29:31 +0100

    Making the direction work

--- components/Maps/Map.tsx
+++ components/Maps/Map.tsx
@@ -18,7 +18,7 @@ const Map = () => {
     if (!origin || !destination) return;
 
     mapRef?.current.fitToSuppliedMarkers(["origin", "destination"], {
-      edgePadding: { top: 50, right: 50, bottm: 50, left: 50 },
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination]);
 
