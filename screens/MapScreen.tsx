00ecff3ee3161b29aa137f8ae8cee97b4b2aec1a
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-17 18:49:55 +0100

    Createing a new component rideOptionCard

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -5,6 +5,7 @@ import tw from "twrnc";
 import Map from "../components/Maps/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NavigateCard from "../components/Navigation/NavigateCard";
+import RideOptionCard from "../components/Ride/RideOptionCard";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +20,11 @@ const MapScreen = () => {
           <Stack.Screen
             name="NavigateCard"
             component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RideOptionCard"
+            component={RideOptionCard}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
