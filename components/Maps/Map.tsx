7296ec21631010aa2c34284d64a66110e720856a
TAlam <tanvir.alam.shawn@gmail.com>
2022-12-18 00:22:53 +0100

    Making the direction work

--- components/Maps/Map.tsx
+++ components/Maps/Map.tsx
@@ -18,7 +18,7 @@ const Map = () => {
     if (!origin || !destination) return;
 
     mapRef?.current.fitToSuppliedMarkers(["origin", "destination"], {
-      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      edgePadding: { top: 50, right: 50, bottm: 50, left: 50 },
     });
   }, [origin, destination]);
 
@@ -63,7 +63,7 @@ const Map = () => {
             longitude: destination.location.lng,
           }}
           title="Destination"
-          description={destination.description}
+          description={origin.description}
           identifier="destination"
         />
       )}
